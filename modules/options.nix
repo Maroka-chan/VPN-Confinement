2ae82316fcac29e8fb2999909a9947db5e6dc5c1
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-10-13 04:24:44 +0200

    add ipv6 options

--- modules/options.nix
+++ modules/options.nix
@@ -1,9 +1,12 @@
 { lib, ... }:
-with lib;
-{
+let
+  inherit (import ../lib/types.nix { inherit lib; }) ipAddress;
+  inherit (lib) mkEnableOption mkOption;
+  inherit (lib.types) listOf str submodule path port enum;
+in {
   options = {
-    enable = mkEnableOption (mdDoc "vpn netns") // {
-      description = mdDoc ''
+    enable = mkEnableOption ("vpn netns") // {
+      description = ''
         Whether to enable the VPN namespace.
 
         To access the networking namespace(netns) a veth pair
@@ -15,22 +18,35 @@ with lib;
     };
 
     accessibleFrom = mkOption {
-      type = types.listOf types.str;
+      type = listOf str;
       default = [];
-      description = mdDoc ''
-        Subnets or specific addresses that the
+      description = ''
+        Subnets, ranges, and specific addresses that the
         namespace should be accessible to.
       '';
       example = [
         "10.0.2.0/24"
         "192.168.1.27"
+        "fd25:9ab6:6133::/64"
+        "fd25:9ab6:6133::203"
       ];
     };
 
     namespaceAddress = mkOption {
-      type = types.str;
+      type = str;
       default = "192.168.15.1";
-      description = mdDoc ''
+      description = ''
+        The address of the veth interface connected to the vpn namespace.
+
+        This is the address used to reach the vpn namespace from other
+        namespaces connected to the linux bridge.
+      '';
+    };
+
+    namespaceAddressIPv6 = mkOption {
+      type = str;
+      default = "fd93:9701:1d00::2";
+      description = ''
         The address of the veth interface connected to the vpn namespace.
 
         This is the address used to reach the vpn namespace from other
@@ -39,9 +55,21 @@ with lib;
     };
 
     bridgeAddress = mkOption {
-      type = types.str;
+      type = str;
       default = "192.168.15.5";
-      description = mdDoc ''
+      description = ''
+        The address of the linux bridge on the default namespace.
+
+        The linux bridge sits on the default namespace and
+        needs an address to make communication between connected
+        namespaces possible, including the default namespace.
+      '';
+    };
+
+    bridgeAddressIPv6 = mkOption {
+      type = str;
+      default = "fd93:9701:1d00::1";
+      description = ''
         The address of the linux bridge on the default namespace.
 
         The linux bridge sits on the default namespace and
@@ -51,17 +79,17 @@ with lib;
     };
 
     openVPNPorts = mkOption {
-      type = with types; listOf (submodule {
+      type = listOf (submodule {
         options = {
           port = mkOption {
             type = port;
-            description = lib.mdDoc "The port to open.";
+            description = "The port to open.";
           };
           protocol = mkOption {
             default = "tcp";
             example = "both";
-            type = types.enum [ "tcp" "udp" "both" ];
-            description = lib.mdDoc "The transport layer protocol to open the ports for.";
+            type = enum [ "tcp" "udp" "both" ];
+            description = "The transport layer protocol to open the ports for.";
           };
         };
       });
@@ -72,28 +100,28 @@ with lib;
     };
 
     portMappings = mkOption {
-      type = with types; listOf (submodule {
+      type = listOf (submodule {
         options = {
           from = mkOption {
             example = 80;
             type = port;
-            description = lib.mdDoc "Port on the default netns.";
+            description = "Port on the default netns.";
           };
           to = mkOption {
             example = 443;
             type = port;
-            description = lib.mdDoc "Port on the VPN netns.";
+            description = "Port on the VPN netns.";
           };
           protocol = mkOption {
             default = "tcp";
             example = "both";
-            type = types.enum [ "tcp" "udp" "both" ];
-            description = lib.mdDoc "The transport layer protocol to open the ports for.";
+            type = enum [ "tcp" "udp" "both" ];
+            description = "The transport layer protocol to open the ports for.";
           };
         };
       });
       default = [];
-      description = mdDoc ''
+      description = ''
         A list of port mappings from
         the host to ports in the namespace.
         Neither the 'to' or 'from' ports should
@@ -110,10 +138,10 @@ with lib;
     };
 
     wireguardConfigFile = mkOption {
-      type = types.path;
+      type = path;
       default = null;
       example = "/secret/wg0.conf";
-      description = mdDoc ''
+      description = ''
         Path to a wg-quick config file.
       '';
     };
