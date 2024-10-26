243596714598ef19255777c9ff8fe7ecc7482c04
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-10-26 22:06:03 +0200

    move tests to its own flake
    Having tests in its own flake means that users will not get the
    dependencies and outputs for the tests when adding the module.

--- flake.nix
+++ flake.nix
@@ -1,22 +1,9 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-  };
-
-  outputs = inputs @ { nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      perSystem = { pkgs, ... }: {
-        checks.interfaces = pkgs.testers.runNixOSTest ./tests/test.nix;
-      };
-
-      flake = {
-        nixosModules = rec {
-          vpnConfinement = ./modules/vpn-netns.nix;
-          default = vpnConfinement;
-        };
-      };
+  outputs = inputs:
+  {
+    nixosModules = rec {
+      vpnConfinement = ./modules/vpn-netns.nix;
+      default = vpnConfinement;
     };
+  };
 }
---
+++ tests/flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ { nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { pkgs, ... }: {
+        checks.interfaces = pkgs.testers.runNixOSTest ./test.nix;
+      };
+    };
+}
