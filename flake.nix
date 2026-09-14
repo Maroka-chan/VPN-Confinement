4fc0ce1d1b11891e82d3bd23bcb1562795e737eb
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2026-09-14 17:57:22 +0200

    remove packages output to avoid pulling in nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
-  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.xz";
-
   outputs = inputs: {
     overlays.default = final: prev: {
-      vpntunnel = inputs.self.packages.${final.system}.default;
+      vpntunnel = prev.callPackage ./pkgs/vpn-tunnel.nix {};
     };
 
     nixosModules = rec {
@@ -13,8 +11,5 @@
       };
       default = vpnConfinement;
     };
-
-    packages.x86_64-linux.default =
-      inputs.nixpkgs.legacyPackages.x86_64-linux.callPackage ./pkgs/vpn-tunnel.nix {};
   };
 }
